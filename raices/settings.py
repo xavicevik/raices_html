065863b543f0b681972fecfcd797c49b587919f0
xavi <jaiver.minotta.h@gmail.com>
2021-09-20 22:22:41 -0500

    Deploy2

--- raices/settings.py
+++ raices/settings.py
@@ -93,17 +93,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     },
-    'default':{
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sipsaeco_raices2',
-        'USER': 'sipsaeco_raices2',
-        'PASSWORD':'M)P)U*R{A=Ru',
-        'HOST': 'www.sipsae.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'sql_mode':'traditional',
-        }
-    }
+
 }
 
 
